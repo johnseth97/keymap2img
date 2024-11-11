a2ec00f24ceb05766822de9a165cae62ad05f533
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2024-11-10 20:48:07 -0500

    gitignore fixed

--- dist/utils/cache.js
+++
@@ -1,5 +0,0 @@
-// src/utils/cache.js
-import NodeCache from 'node-cache';
-// Cache TTL set to 24 hours (86400 seconds). Adjust as needed.
-const cache = new NodeCache({ stdTTL: 86400, checkperiod: 120 });
-export default cache;
